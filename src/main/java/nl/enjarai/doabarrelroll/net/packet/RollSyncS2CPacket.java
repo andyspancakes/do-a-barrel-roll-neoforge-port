6f2c16acec83b597d791f9cb14c113c1e8f6d9cd
andyspancakes <port@doabarrelroll.dev>
2026-04-07 05:54:26 +0000

    Fix duplicate roll_sync packet ID: rename S2C to roll_sync_s2c

--- src/main/java/nl/enjarai/doabarrelroll/net/packet/RollSyncS2CPacket.java
+++ src/main/java/nl/enjarai/doabarrelroll/net/packet/RollSyncS2CPacket.java
@@ -7,7 +7,7 @@ import net.minecraft.network.packet.CustomPayload;
 import nl.enjarai.doabarrelroll.DoABarrelRoll;
 
 public record RollSyncS2CPacket(int entityId, boolean rolling, float roll) implements CustomPayload {
-    public static final Id<RollSyncS2CPacket> PACKET_ID = new Id<>(DoABarrelRoll.id("roll_sync"));
+    public static final Id<RollSyncS2CPacket> PACKET_ID = new Id<>(DoABarrelRoll.id("roll_sync_s2c"));
     public static final PacketCodec<ByteBuf, RollSyncS2CPacket> PACKET_CODEC = PacketCodec.tuple(
             PacketCodecs.INTEGER, RollSyncS2CPacket::entityId,
             PacketCodecs.BOOLEAN, RollSyncS2CPacket::rolling,
